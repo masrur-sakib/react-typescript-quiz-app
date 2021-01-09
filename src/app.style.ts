a33f6a204d5158649691586eb700b10de282a96e
Masrur Sakib <mashrur333@gmail.com>
2021-01-09 12:30:10 +0600

    Made the application mobile responsive

--- src/app.style.ts
+++ src/app.style.ts
@@ -23,7 +23,7 @@ export const GlobalStyle = createGlobalStyle`
 `;
 
 export const Wrapper = styled.div`
-    width: 400px;
+    width: 350px;
     display: flex;
     flex-direction: column;
     align-items: left;
